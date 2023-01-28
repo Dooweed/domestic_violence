3ba8b6fb050d54eefe72bc1f871e4845f86b3118
Alkamyan David <ada_170301@mail.ru>
2023-01-29 00:58:19 +0500

    Minor changes

--- domestic_violence/settings.py
+++ domestic_violence/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7t$rtp=2b&5awc21oa!u7i0u*jy2o%gqy0b(o%(tq#vlfdnldd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['104.248.207.13', 'localhost']
 
 
 # Application definition
