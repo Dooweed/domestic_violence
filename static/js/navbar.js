b42da399c85dd942896ff9561c61856323e862aa
Alkamyan David <ada_170301@mail.ru>
2023-01-29 06:48:14 +0500

    Minor changes

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,8 +1,10 @@
 const sheltersBtn = document.getElementById('sheltersBtn');
 sheltersBtn.addEventListener('click', function (e) {
+    e.preventDefault();
     navigator.geolocation.getCurrentPosition((position) => {
         window.location = sheltersBtn.getAttribute('data-url') + `?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`
     })
+    return false;
 })
 
 document.querySelector('.navbar-toggler').addEventListener('click', (e) => {
